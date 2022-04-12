474c8854630d9dd1786e04b52c8594951325ba94
Aleksandr Shishkov <aleksandr.shishkov@student.um.si>
2022-04-12 21:38:49 +0200

    added endpoints for login and register

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('users index');
 });
+
+router.post('/login', function(req, res, next) {
+    if (req.body.username == 'admin' && req.body.password == 'admin')
+        res.status(200).send('Success login');
+    else
+        res.status(401).send('Wrong username or password');
+})
+
+router.post('/register', function(req, res, next) {
+    if (req.body.username == 'admin' && req.body.password == 'admin')
+        res.status(200).send('Success register');
+    else
+        res.status(401).send('User already exists');
+})
 
 module.exports = router;
