919b045a5415af3063ef7f30cf80e40a829f92f7
Aleksandr Shishkov <aleksandr.shishkov@student.um.si>
2022-05-02 15:30:12 +0200

    remove git ignore files, improved auth and register

--- app.js
+++ app.js
@@ -3,15 +3,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
-require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  console.log('addr: ' + add);
-})
+var UserRoutes = require('./routes/UserRoutes');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+console.log(results);
 
 var app = express();
+
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGO_URL;
+mongoose.connect(mongoDB);
+mongoose.connection.on('error', err => {
+    console.log(err);
+});
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo');
+app.use(session({
+    secret: 'project2020as',
+    resave: true,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: mongoDB })
+}));
+
+var indexRouter = require('./routes/index');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,15 +56,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', UserRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/UserRoutes.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var UserController = require('../controllers/UserController.js');
+
+const multer = require('multer')
+const upload = multer({ dest: 'public/images' })
+
+/*
+ * GET
+ */
+router.get('/', UserController.auth, UserController.list);
+
+router.get('/auth', UserController.auth, function(req, res) { return res.status(200).send('OK'); });
+
+/*
+ * GET
+ */
+router.get('/:id', UserController.show);
+/*
+ * POST
+ */
+router.post('/', UserController.is_exists, UserController.create);
+
+router.post('/register', UserController.is_exists, UserController.create);
+router.post('/login', upload.single('image'), UserController.login);
+
+/*
+ * PUT
+ */
+router.put('/:id', UserController.update);
+
+/*
+ * DELETE
+ */
+router.delete('/:id', UserController.remove);
+
+module.exports = router;
